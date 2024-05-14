54f2c5bf10f1c907a56013ff4b1e91e5c722474e
CheryX <maciejmusielik@gmail.com>
2024-05-14 21:45:34 +0200

    Add chemical elements data

---
+++ src/pages/projects/example-data/elements.js
@@ -0,0 +1,474 @@
+export default [
+  {
+    "back": "Hydrogen",
+    "front": "H"
+  },
+  {
+    "back": "Helium",
+    "front": "He"
+  },
+  {
+    "back": "Lithium",
+    "front": "Li"
+  },
+  {
+    "back": "Beryllium",
+    "front": "Be"
+  },
+  {
+    "back": "Boron",
+    "front": "B"
+  },
+  {
+    "back": "Carbon",
+    "front": "C"
+  },
+  {
+    "back": "Nitrogen",
+    "front": "N"
+  },
+  {
+    "back": "Oxygen",
+    "front": "O"
+  },
+  {
+    "back": "Fluorine",
+    "front": "F"
+  },
+  {
+    "back": "Neon",
+    "front": "Ne"
+  },
+  {
+    "back": "Sodium",
+    "front": "Na"
+  },
+  {
+    "back": "Magnesium",
+    "front": "Mg"
+  },
+  {
+    "back": "Aluminum",
+    "front": "Al"
+  },
+  {
+    "back": "Silicon",
+    "front": "Si"
+  },
+  {
+    "back": "Phosphorus",
+    "front": "P"
+  },
+  {
+    "back": "Sulfur",
+    "front": "S"
+  },
+  {
+    "back": "Chlorine",
+    "front": "Cl"
+  },
+  {
+    "back": "Argon",
+    "front": "Ar"
+  },
+  {
+    "back": "Potassium",
+    "front": "K"
+  },
+  {
+    "back": "Calcium",
+    "front": "Ca"
+  },
+  {
+    "back": "Scandium",
+    "front": "Sc"
+  },
+  {
+    "back": "Titanium",
+    "front": "Ti"
+  },
+  {
+    "back": "Vanadium",
+    "front": "V"
+  },
+  {
+    "back": "Chromium",
+    "front": "Cr"
+  },
+  {
+    "back": "Manganese",
+    "front": "Mn"
+  },
+  {
+    "back": "Iron",
+    "front": "Fe"
+  },
+  {
+    "back": "Cobalt",
+    "front": "Co"
+  },
+  {
+    "back": "Nickel",
+    "front": "Ni"
+  },
+  {
+    "back": "Copper",
+    "front": "Cu"
+  },
+  {
+    "back": "Zinc",
+    "front": "Zn"
+  },
+  {
+    "back": "Gallium",
+    "front": "Ga"
+  },
+  {
+    "back": "Germanium",
+    "front": "Ge"
+  },
+  {
+    "back": "Arsenic",
+    "front": "As"
+  },
+  {
+    "back": "Selenium",
+    "front": "Se"
+  },
+  {
+    "back": "Bromine",
+    "front": "Br"
+  },
+  {
+    "back": "Krypton",
+    "front": "Kr"
+  },
+  {
+    "back": "Rubidium",
+    "front": "Rb"
+  },
+  {
+    "back": "Strontium",
+    "front": "Sr"
+  },
+  {
+    "back": "Yttrium",
+    "front": "Y"
+  },
+  {
+    "back": "Zirconium",
+    "front": "Zr"
+  },
+  {
+    "back": "Niobium",
+    "front": "Nb"
+  },
+  {
+    "back": "Molybdenum",
+    "front": "Mo"
+  },
+  {
+    "back": "Technetium",
+    "front": "Tc"
+  },
+  {
+    "back": "Ruthenium",
+    "front": "Ru"
+  },
+  {
+    "back": "Rhodium",
+    "front": "Rh"
+  },
+  {
+    "back": "Palladium",
+    "front": "Pd"
+  },
+  {
+    "back": "Silver",
+    "front": "Ag"
+  },
+  {
+    "back": "Cadmium",
+    "front": "Cd"
+  },
+  {
+    "back": "Indium",
+    "front": "In"
+  },
+  {
+    "back": "Tin",
+    "front": "Sn"
+  },
+  {
+    "back": "Antimony",
+    "front": "Sb"
+  },
+  {
+    "back": "Tellurium",
+    "front": "Te"
+  },
+  {
+    "back": "Iodine",
+    "front": "I"
+  },
+  {
+    "back": "Xenon",
+    "front": "Xe"
+  },
+  {
+    "back": "Cesium",
+    "front": "Cs"
+  },
+  {
+    "back": "Barium",
+    "front": "Ba"
+  },
+  {
+    "back": "Lanthanum",
+    "front": "La"
+  },
+  {
+    "back": "Cerium",
+    "front": "Ce"
+  },
+  {
+    "back": "Praseodymium",
+    "front": "Pr"
+  },
+  {
+    "back": "Neodymium",
+    "front": "Nd"
+  },
+  {
+    "back": "Promethium",
+    "front": "Pm"
+  },
+  {
+    "back": "Samarium",
+    "front": "Sm"
+  },
+  {
+    "back": "Europium",
+    "front": "Eu"
+  },
+  {
+    "back": "Gadolinium",
+    "front": "Gd"
+  },
+  {
+    "back": "Terbium",
+    "front": "Tb"
+  },
+  {
+    "back": "Dysprosium",
+    "front": "Dy"
+  },
+  {
+    "back": "Holmium",
+    "front": "Ho"
+  },
+  {
+    "back": "Erbium",
+    "front": "Er"
+  },
+  {
+    "back": "Thulium",
+    "front": "Tm"
+  },
+  {
+    "back": "Ytterbium",
+    "front": "Yb"
+  },
+  {
+    "back": "Lutetium",
+    "front": "Lu"
+  },
+  {
+    "back": "Hafnium",
+    "front": "Hf"
+  },
+  {
+    "back": "Tantalum",
+    "front": "Ta"
+  },
+  {
+    "back": "Wolfram",
+    "front": "W"
+  },
+  {
+    "back": "Rhenium",
+    "front": "Re"
+  },
+  {
+    "back": "Osmium",
+    "front": "Os"
+  },
+  {
+    "back": "Iridium",
+    "front": "Ir"
+  },
+  {
+    "back": "Platinum",
+    "front": "Pt"
+  },
+  {
+    "back": "Gold",
+    "front": "Au"
+  },
+  {
+    "back": "Mercury",
+    "front": "Hg"
+  },
+  {
+    "back": "Thallium",
+    "front": "Tl"
+  },
+  {
+    "back": "Lead",
+    "front": "Pb"
+  },
+  {
+    "back": "Bismuth",
+    "front": "Bi"
+  },
+  {
+    "back": "Polonium",
+    "front": "Po"
+  },
+  {
+    "back": "Astatine",
+    "front": "At"
+  },
+  {
+    "back": "Radon",
+    "front": "Rn"
+  },
+  {
+    "back": "Francium",
+    "front": "Fr"
+  },
+  {
+    "back": "Radium",
+    "front": "Ra"
+  },
+  {
+    "back": "Actinium",
+    "front": "Ac"
+  },
+  {
+    "back": "Thorium",
+    "front": "Th"
+  },
+  {
+    "back": "Protactinium",
+    "front": "Pa"
+  },
+  {
+    "back": "Uranium",
+    "front": "U"
+  },
+  {
+    "back": "Neptunium",
+    "front": "Np"
+  },
+  {
+    "back": "Plutonium",
+    "front": "Pu"
+  },
+  {
+    "back": "Americium",
+    "front": "Am"
+  },
+  {
+    "back": "Curium",
+    "front": "Cm"
+  },
+  {
+    "back": "Berkelium",
+    "front": "Bk"
+  },
+  {
+    "back": "Californium",
+    "front": "Cf"
+  },
+  {
+    "back": "Einsteinium",
+    "front": "Es"
+  },
+  {
+    "back": "Fermium",
+    "front": "Fm"
+  },
+  {
+    "back": "Mendelevium",
+    "front": "Md"
+  },
+  {
+    "back": "Nobelium",
+    "front": "No"
+  },
+  {
+    "back": "Lawrencium",
+    "front": "Lr"
+  },
+  {
+    "back": "Rutherfordium",
+    "front": "Rf"
+  },
+  {
+    "back": "Dubnium",
+    "front": "Db"
+  },
+  {
+    "back": "Seaborgium",
+    "front": "Sg"
+  },
+  {
+    "back": "Bohrium",
+    "front": "Bh"
+  },
+  {
+    "back": "Hassium",
+    "front": "Hs"
+  },
+  {
+    "back": "Meitnerium",
+    "front": "Mt"
+  },
+  {
+    "back": "Darmstadtium",
+    "front": "Ds"
+  },
+  {
+    "back": "Roentgenium",
+    "front": "Rg"
+  },
+  {
+    "back": "Copernicium",
+    "front": "Cn"
+  },
+  {
+    "back": "Nihonium",
+    "front": "Nh"
+  },
+  {
+    "back": "Flerovium",
+    "front": "Fl"
+  },
+  {
+    "back": "Moscovium",
+    "front": "Mc"
+  },
+  {
+    "back": "Livermorium",
+    "front": "Lv"
+  },
+  {
+    "back": "Tennessine",
+    "front": "Ts"
+  },
+  {
+    "back": "Oganesson",
+    "front": "Og"
+  }
+]
